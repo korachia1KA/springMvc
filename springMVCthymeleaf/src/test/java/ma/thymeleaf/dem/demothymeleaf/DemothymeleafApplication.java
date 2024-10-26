a7a3673c7dc865bdff5578f1f7be16fd1d221321
korachia1KA <abdelghafourkorachi9@gmail.com>
2024-10-26 21:39:48 +0100

    Add files via upload

---
+++ springMVCthymeleaf/src/test/java/ma/thymeleaf/dem/demothymeleaf/DemothymeleafApplication.java
@@ -0,0 +1,11 @@
+package ma.thymeleaf.dem.demothymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemothymeleafApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemothymeleafApplication.class, args);
+    }
+}
